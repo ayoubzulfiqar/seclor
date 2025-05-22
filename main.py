8c646e20138aa49f498276428cadf77f917f2861
ayoubzulfiqar <ayoubzulfiqar3@gmail.com>
2025-05-22 11:43:28 -0700

    main

---
+++ main.py
@@ -0,0 +1,32 @@
+import asyncio
+
+from spider.secer import SecEdgerAsync
+
+
+async def main():
+    print("Running SEC Edger....")
+    # APPLE INC
+    await SecEdgerAsync(companyName="Tesla")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+# 10-K, 10-Q, 8-K, DEF 14A, F-4, S-1, SC 13D, SC 13G
+# 10-K, 10-Q, 8-K, DEF 14A, F-4, S-1, SC 13D, SC 13G
+
+
+# # Run multiple company searches concurrently
+# async def main():
+#     # More companies here
+#     tasks = [
+#         SecEdgerAsync("Apple Inc."),
+#         SecEdgerAsync("Microsoft"),
+#         SecEdgerAsync("Tesla"),
+#         SecEdgerAsync("Google"),
+#     ]
+#     await asyncio.gather(*tasks)
+
+
+# # Start the async event loop
+# if __name__ == "__main__":
+#     asyncio.run(main())
